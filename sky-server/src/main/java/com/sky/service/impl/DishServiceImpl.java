4e461e981049464c6c9fce55fa37cbdb632a8c77
tangfire <52628216@qq.com>
2024-12-20 22:02:37 +0800

    insert dishwithFlavor!

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -6,6 +6,7 @@ import com.sky.entity.Dish;
 import com.sky.mapper.DishMapper;
 import com.sky.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,9 @@ public class DishServiceImpl implements DishService {
     public void saveWithFlavor(DishDTO dishDTO) {
         Dish dish = new Dish();
 
+        BeanUtils.copyProperties(dishDTO, dish);
+
+        // 向菜品表插入1条数据
 
 
     }
