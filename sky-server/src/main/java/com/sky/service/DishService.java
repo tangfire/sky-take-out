54d9339bebbfce1f1613cb6f8323bf538eed8978
tangfire <52628216@qq.com>
2024-12-20 21:56:54 +0800

    insert dish

---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,18 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public interface DishService {
+
+    /**
+     * 新增菜品和对应的口味
+     * @param dishDTO
+     */
+    public void saveWithFlavor(DishDTO dishDTO);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,21 @@
+package com.sky.service.impl;
+
+
+import com.sky.dto.DishDTO;
+import com.sky.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DishServiceImpl implements DishService {
+
+    /**
+     * 新增菜品和对应口味
+     * @param dishDTO
+     */
+    @Override
+    public void saveWithFlavor(DishDTO dishDTO) {
+
+    }
+}
